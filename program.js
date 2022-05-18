364260a92f69d5208fde30612073f6e60f28fce5
anonymoustafa <anonymoustafa@outlook.com>
2022-05-18 20:57:48 +0430

    finished

--- program.js
+++ program.js
@@ -1,17 +1,28 @@
-import chalk from 'chalk';
+import chalk from "chalk";
 import Jimp from "jimp";
 import yargs from "yargs/yargs";
 import { hideBin } from "yargs/helpers";
 const argv = yargs(hideBin(process.argv)).argv;
 const error = chalk.bold.red;
-const warning = chalk.hex('#FFA500'); // Orange color
+const warning = chalk.hex("#FFA500"); // Orange color
 const dateAndTime = new Date();
-if(!argv.dimension) warning("Dimensions is not specified!. Run $/usr/bin/env node program.js --dimension=YouTube or arbitrary values like: --dimension=1270*768 to change the selected file.");
+if (!argv.dimension)
+        warning(
+                "Dimensions is not specified!. Run $/usr/bin/env node program.js --dimension=YouTube or arbitrary values like: --dimension=1270*768 to change the selected file."
+        );
+if (argv.dimension.toLocaleLowerCase() == "youtube") {
+        var width = 1280,
+                height = 720;
+} else {
+        const widthAndHeight = argv.dimension.split("*");
+        var width = widthAndHeight[0],
+                height = widthAndHeight[1];
+}
 
-if(!argv.format) warning("Format is not specified!. Run $/usr/bin/env node program.js --format=jpg or png, tiff, bmp, git. If not specifies, png would automatically be assigned.");
-
-const widthAndHeight = argv.dimension.split('*');
-
+if (!argv.format)
+        warning(
+                "Format is not specified!. Run $/usr/bin/env node program.js --format=jpg or png, tiff, bmp, git. If not specifies, png would automatically be assigned."
+        );
 
 if (argv.format == "jpg") {
         const format = "jpg";
@@ -28,8 +39,23 @@ if (argv.format == "gif") {
         const format = "png";
 }
 
-
-const newName =dateAndTime.getFullYear().toString()+dateAndTime.getMonth()+dateAndTime.getDate()+dateAndTime.getHours()+dateAndTime.getMinutes()+dateAndTime.getSeconds()+dateAndTime.getMilliseconds().toString()+"."+argv.format;
+const newName =
+        dateAndTime.getFullYear().toString() +
+        dateAndTime.getMonth() +
+        dateAndTime.getDate() +
+        dateAndTime.getHours() +
+        dateAndTime.getMinutes() +
+        dateAndTime.getSeconds() +
+        dateAndTime.getMilliseconds().toString() +
+        "." +
+        argv.format;
 console.log(newName);
 console.log(argv);
 console.log(argv._.toString());
+
+Jimp.read(argv._.toString(), (err, picture) => {
+        if (err) throw err;
+        picture.resize(width, height) // resize
+                .quality(60) // set JPEG quality
+                .write(newName); // save
+});
